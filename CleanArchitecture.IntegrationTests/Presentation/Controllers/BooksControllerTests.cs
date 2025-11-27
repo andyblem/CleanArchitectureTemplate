0ef6fe33e45f68a3b7519d37ea129bd278ce68b8
Andyblem <andyblem@gmail.com>
2025-11-27 17:17:12 +0200

    Setup testing project

---
+++ CleanArchitecture.IntegrationTests/Presentation/Controllers/BooksControllerTests.cs
@@ -0,0 +1,28 @@
+using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace CleanArchitecture.ApiTests.Controllers
+{
+    public class BooksControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public BooksControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task SwaggerJson_Is_Available()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("/swagger/v1/swagger.json");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var json = await response.Content.ReadAsStringAsync();
+            json.Should().Contain("\"openapi\"");
+        }
+    }
+}
---
+++ CleanArchitecture.UnitTests/Application/Mappings/BookProfilesTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.UnitTests.Application.Mappings
+{
+    internal class BookProfilesTests
+    {
+    }
+}
